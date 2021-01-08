9c0b8f60d626dfd87b0a09cdbf3d8f626db32d87
Aarti13 <37829907+Aarti13@users.noreply.github.com>
2021-01-08 20:25:40 +0530

    Update CountSort.java

--- CountSort.java
+++ CountSort.java
@@ -3,9 +3,11 @@ import java.util.*;
 
 public class Main {
 
+// we perform count sort for those elements woh doesnot vary too much like jeemains marks 
   public static void countSort(int[] arr, int min, int max) {
    
-   int n = max - min + 1 ;
+   int n = max - min + 1 ; //size of frequency matrix ie 
+    // frst find smallest and largest value from array
    int[] freq = new int[n]; // frequency of array element
    
    for( int i =0 ; i< arr.length ; i++){
@@ -14,13 +16,15 @@ public class Main {
    }
    
    // WHY ???? because we have to make our count sort stable
+    // ie to preserve their order
     //Prefix sum array for frequency matrix
    for(int i =1 ; i< n ; i++){
        freq[i] += freq[i-1];
    }
    
     int[] ans = new int[arr.length]; // Ans array
-
+  
+    // aakhri value kha tk aaega ie aakhri 3 konse index pe aaega
    for(int i =arr.length-1 ; i>=0 ; i--){
        int val = arr[i]-min ;
        ans[ freq[val ]-1 ] = arr[i];
